e67f93eece4e0a7cc09014a0a0cf784708d6bf6e
jpalacioco <50222427+jpalacioco@users.noreply.github.com>
2019-06-04 15:41:51 -0500

    q01 terminado

--- q01.py
+++ q01.py
@@ -1,4 +1,11 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
-#
+import csv
+columna_2=0
+data_lab_03=open('data.csv')
+separador = "\t"
+for fila in data_lab_03:
+    columna_2 = int(fila.split(separador)[1].strip())+int(columna_2)
+print(columna_2)
+data_lab_03.close()
